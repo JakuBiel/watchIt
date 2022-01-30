444a7031b298c138229ee6fa33405b053c9bb064
Kuba Biel <jakubiel@o2.pl>
2022-01-30 23:23:40 +0100

    5

--- watchit.js
+++ watchit.js
@@ -9,22 +9,28 @@ program
 	.version("0.0.1")
 	.argument("[filename]", "Name a file to execute")
 	.action(async ({ filename }) => {
-		const name = filename || watchit.js;
+		const name = filename || "watchit.js";
 
 		try {
-			await fs.promise.access(name);
+			await fs.promises.access(name);
 		} catch (err) {
-			throw new Error(`Could not find the file "${name}"`);
+			throw new Error(`Could not find the file ${name}`);
 		}
-		const start = debounce(() => {
-			spawn("node", [name], { stdio: "inherit" });
-		}, 100);
-	});
 
-chokidar
-	.watch(".")
-	.on("add", () => start)
-	.on("change", () => start)
-	.on("unlink", () => start);
+		let proc;
+		const start = debounce(() => {
+			if (proc) {
+				proc.kill();
+				console.log(">>> New process started..<<<");
+			}
+			proc = spawn("node", [name], { stdio: "inherit" });
+		}, 500);
+
+		chokidar
+			.watch(".")
+			.on("add", start)
+			.on("change", start)
+			.on("unlink", start);
+	});
 
 program.parse(process.argv);
