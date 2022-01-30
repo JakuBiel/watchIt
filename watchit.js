a49731ae4a6774e0a651dcac84823a4325c01f74
Kuba Biel <jakubiel@o2.pl>
2022-01-30 22:30:06 +0100

    s

--- watchit.js
+++ watchit.js
@@ -2,22 +2,29 @@
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
 const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
 
 program
 	.version("0.0.1")
 	.argument("[filename]", "Name a file to execute")
-	.action((args) => {
-		console.log(args);
+	.action(async ({ filename }) => {
+		const name = filename || watchit.js;
+
+		try {
+			await fs.promise.access(name);
+		} catch (err) {
+			throw new Error(`Could not find the file "${name}"`);
+		}
+		const start = debounce(() => {
+			spawn("node", [name], { stdio: "inherit" });
+		}, 100);
 	});
 
+chokidar
+	.watch(".")
+	.on("add", () => start)
+	.on("change", () => start)
+	.on("unlink", () => start);
+
 program.parse(process.argv);
-
-// const start = debounce(() => {
-// 	console.log("Start program");
-// }, 100);
-
-// chokidar
-// 	.watch(".")
-// 	.on("add", () => start)
-// 	.on("change", () => console.log("File Changed!"))
-// 	.on("unlink", () => console.log("File Unlinked!"));
