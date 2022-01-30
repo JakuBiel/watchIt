cb2ea040ed73b4e4e76c32012aae48384476fe65
Kuba Biel <jakubiel@o2.pl>
2022-01-30 17:05:01 +0100

    first commit

---
+++ watchit.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+
+const start = debounce(() => {
+	console.log("Start program");
+}, 100);
+
+chokidar
+	.watch(".")
+	.on("add", () => start)
+	.on("change", () => console.log("File Changed!"))
+	.on("unlink", () => console.log("File Unlinked!"));
