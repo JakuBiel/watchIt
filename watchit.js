b5a2f665bc7b952a53bc2657b9a791f94c100d03
Kuba Biel <jakubiel@o2.pl>
2022-01-30 17:17:58 +0100

    s

--- watchit.js
+++ watchit.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 const debounce = require("lodash.debounce");
 const chokidar = require("chokidar");
+const program = require("caporal");
 
-const start = debounce(() => {
-	console.log("Start program");
-}, 100);
+program
+	.version("0.0.1")
+	.argument("[filename]", "Name a file to execute")
+	.action((args) => {
+		console.log(args);
+	});
 
-chokidar
-	.watch(".")
-	.on("add", () => start)
-	.on("change", () => console.log("File Changed!"))
-	.on("unlink", () => console.log("File Unlinked!"));
+program.parse(process.argv);
+
+// const start = debounce(() => {
+// 	console.log("Start program");
+// }, 100);
+
+// chokidar
+// 	.watch(".")
+// 	.on("add", () => start)
+// 	.on("change", () => console.log("File Changed!"))
+// 	.on("unlink", () => console.log("File Unlinked!"));
